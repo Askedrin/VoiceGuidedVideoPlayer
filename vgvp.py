4a9aa5acc6ba1de479c3096a6aa2ddfce00ac4e7
Askedrin <i.zellagui@esi-sba.dz>
2023-08-12 20:31:44 +0100

    Finished the voice command for asking according to what exists

--- vgvp.py
+++ vgvp.py
@@ -1,6 +1,6 @@
 import configparser
 import os
-
+import time
 
 import pyttsx3
 
@@ -8,11 +8,6 @@ engine = pyttsx3.init()
 
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-50)
-
-engine.say('Sally sells seashells by the seashore.')
-engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
-
 
 
 # Define the path to the config.conf file
@@ -31,15 +26,26 @@ print("PARAMETER_1:", files_path)
 print(os.getcwd())
 
 sources = ["Anime", "Series", "Movies"]
+path_existence=[]
 for s in sources:
     filepath = os.path.join(files_path, s)
     if (os.path.exists(filepath)):
         print(s+" exists.")
         if not (os.path.isfile(filepath)):
             print(" And it is indeed a folder.")
+            path_existence.append(s)
         else:
             print(" But it is not a folder.")       
     else:
         print(s+ " doesn't exist")
         # print("Which of the folders would you like to create?")
 
+
+engine.say('The existing folders are the following: ')
+time.sleep(0.5)
+for pe in path_existence:
+    engine.say(pe)
+    time.sleep(0.5)
+time.sleep(0.5)
+engine.say('What would you like to watch ?')
+engine.runAndWait()
